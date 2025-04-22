492ea24a41894e94669d28c311b7c8a8c273c09b
Kanhaiya shaw <kanhaiyashaw2246@gmail.com>
2025-04-22 19:53:09 +0530

    first commit

---
+++ additive-number_306.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAdditiveNumber(self, num: str) -> bool:
+        if len(num) < 3:
+            return False
+        
+        indx1 = 0
+        
+            
+
+
+
+demo = Solution()
+
+print(demo.isAdditiveNumber('199100199'))
