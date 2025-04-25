a96cf94e084b4cbdc028ea013fb2ac2a9c4d1e8a
Kanhaiya shaw <kanhaiyashaw2246@gmail.com>
2025-04-25 15:30:59 +0530

    Day-4

---
+++ longest-common-prefix_14.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(len(strs)):
+            # temp pass
+
+            pass
+            
+
+
+            
+
+
+demo = Solution()
+
+print(demo.longestCommonPrefix(["flower","flow","flight"]))
